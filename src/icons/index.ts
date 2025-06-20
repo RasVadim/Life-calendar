7351405ae56b94d5e3f78284102ff27dc4fa700f
Vadim Rasstrigin <Rasstriginv@gmail.com>
2025-06-20 14:14:44 +0200

    Fix outlineProfile import issue by renaming file

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -16,7 +16,7 @@ export { YearsIcon } from './yearsIcon';
 
 export { MonthsIcon } from './monthsIcon';
 
-export { OutlineProfile } from './outlineProfile';
+export { OutlineProfile } from './outlineProfileIcon';
 
 export { ChevronIcon } from './chevronIcon';
 
