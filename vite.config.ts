76c18055db7f1997aba188dee74e9da543eab176
Vadim Rasstrigin <Rasstriginv@gmail.com>
2025-06-21 22:54:09 +0200

    added logo icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,62 +1,61 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-import { VitePWA } from "vite-plugin-pwa";
-import mkcert from "vite-plugin-mkcert";
-import path from "path";
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
   },
   plugins: [
     react(),
     mkcert(),
     VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
-      outDir: "dist",
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      outDir: 'dist',
       manifest: {
-        name: "Life Calendar",
-        short_name: "LC",
-        description: "Life Calendar for your life",
-        display: "fullscreen",
+        name: 'Life Calendar',
+        short_name: 'LC',
+        description: 'Calendar for your all life',
+        display: 'fullscreen',
         icons: [
           {
-            src: "images/pwa-64x64.png",
-            sizes: "64x64",
-            type: "image/png",
+            src: 'images/pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
           },
           {
-            src: "images/pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
+            src: 'images/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
           {
-            src: "images/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any",
+            src: 'images/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
           },
           {
-            src: "images/maskable-icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
+            src: 'images/maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
           },
         ],
         screenshots: [
           {
-            src: "images/desktop-screenshot.png",
-            sizes: "1920x1080",
-            type: "image/png",
-            form_factor: "wide",
+            src: 'images/desktop-screenshot.png',
+            sizes: '1920x1080',
+            type: 'image/png',
+            form_factor: 'wide',
           },
           {
-            src: "images/mobile-screenshot.png",
-            sizes: "375x812",
-            type: "image/png",
+            src: 'images/mobile-screenshot.png',
+            sizes: '375x812',
+            type: 'image/png',
           },
         ],
       },
@@ -64,7 +63,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
